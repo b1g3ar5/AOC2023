66fa4c3d55bfba042bdf30a4523b43e7294342ec
Nick Straw <nick@broxholme.com>
2023-12-09 15:05:59 +0000

    Day9

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,18 +1,20 @@
 module Day9(day9) where
 
-import Utils
+import Utils (getLines)
 
 
-parse :: String -> Int
-parse = read
+extrap :: [Int] -> Int
+extrap ys = if head ys == 0 then 0 else last ys + extrap (zipWith (-) (tail ys) ys)
 
 
 day9 :: IO ()
 day9 = do
-  ss <- getLines 9
-  let g = parse <$> ss
+  ls <- getLines 9
+  let g :: [[Int]]
+      g = (read <$>) . words <$> ls
 
-  putStrLn $ "Day9: part1: " ++ show g
-  putStrLn $ "Day9: part2: " ++ show ""
+  putStrLn $ "Day9: part2: " ++ show (sum $ extrap <$> g)
+  putStrLn $ "Day9: part2: " ++ show (sum $ extrap . reverse <$> g)
 
   return ()
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -13,7 +13,7 @@ import Day5
 import Day6
 import Day7
 import Day8
---import Day9
+import Day9
 --import Day10
 --import Day11
 --import Day12
@@ -33,14 +33,17 @@ import Day8
 
 libMain :: IO ()
 libMain = do
-  day1
-  day2
-  day3
-  day4
-  day5
-  day6
-  day7
-  day8
+  --day1
+  --day2
+  --day3
+  --day4
+  --day5
+  --day6
+  --day7
+  --day8
+  day9
+
+
 
 
 
